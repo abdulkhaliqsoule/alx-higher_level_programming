b56b6542eb8e1367d8299285f7cc322ecbda85b6
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2022-12-04 20:10:49 +0000

    int

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """Print integers from a list
+    Args:
+        my_list: list to print integers
+
+    Returns:
+        None
+    """
+    for integer in my_list:
+        print("{}".format(integer))
