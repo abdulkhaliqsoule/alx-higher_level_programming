6173a0448927199547ade68406a0b75e98a97fb3
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2023-01-10 22:22:01 +0000

    file i/o

---
+++ 0x0A-python-inheritance/0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+from_json_string function
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """returns an object (Python data structure)
+    represented by a JSON string"""
+
+    return (json.loads(my_str))
