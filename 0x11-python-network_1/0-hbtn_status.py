1983949f5c0348597f5a0cfa4ad912785aacd341
Abdul Khaliq <abdulkhaliqsoule@gmail.com>
2023-04-01 05:28:52 +0000

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-# fetches website
+""" Python script that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 
-if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        content = response.read()
-        url = content.decode('utf-8')
-        typed = response.info()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(url))
+
+if __name__ == '__main__':
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        html = res.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
